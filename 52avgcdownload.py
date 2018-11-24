7941b35e05d90a299c3701ae278ee4d0801e119e
Three专属 <946563239@qq.com>
2018-11-24 19:25:01 +0800

    删除了原来的下载脚本，只保留多线程下载脚本

--- 52avgcdownload.py
+++ 52avgcdownload.py
@@ -1,74 +1,84 @@
 # -*- coding: utf-8 -*-
-import requests,os,sqlite3,time
+import requests,os,sqlite3,time,re,threading
 from contextlib import closing
 
-def download_file(url0, path0,newpath):
-	with open(path0, "wb") as f:
-		for i in range(6000):
-			url = url0 + str(i) + '.ts'
+def download_file(video_m3u8, oldpath , newpath):
+	with open(oldpath, "wb") as f:
+		i = 0
+		while True:
+			url = video_m3u8[0:-5] + str(i) + '.ts'		#对m3u8链接切片，构造ts
 			print(url)
-			with closing(requests.get(url, stream=True)) as r:
-				chunk_size = 1024
-				content_size = int(r.headers['content-length'])
-				if (content_size > 30):
-					#print("下载第" + str(i) + "段，大小为:" + str(content_size))
-					n = 1
-					for chunk in r.iter_content(chunk_size=chunk_size):
-						loaded = n * 1024.0 / content_size
-						f.write(chunk)
-						f.flush()
-						os.fsync(f.fileno())
-						n += 1
-				else:
-					#comm0 = "ffmpeg -i {0} -acodec copy -vcodec copy -f mp4 -bsf:a aac_adtstoasc {1} -y".format(str(path0),str(newpath))	#转码生成新文件
-					#os.system(comm0)
-					#time.sleep(2)
-					#while True:
-					#	comm1 = 'onedrivecmd put "{0}" od:/'.format(str(newpath))
-					#	print(comm1)
-					#	res = os.popen(comm1)
-					#	if str(list(res)) != "Annotations must be specified before other elements in a JSON object":
-					#		break
+			with closing(requests.get(url, stream=True)) as r:	 #r对应一个ts完整请求
+				content_size = int(r.headers['content-length'])		#获取ts大小
+				if (content_size < 50):
 					break
+				for chunk in r.iter_content(chunk_size=1024):		#对ts大小按1024进行存到硬盘
+					f.write(chunk)
+					f.flush()
+					os.fsync(f.fileno())
+			i = i+1			#存完一个ts，进行下一个ts
 
-if __name__ == '__main__':
+def handle_file(video_m3u8, oldpath , newpath):
+	#1.数据库处理
 	conn = sqlite3.connect("ziyuan.db")
 	c = conn.cursor()
-	res0 = c.execute("select * from ziyuanlist")
-	lis=[]
-	for u in res0:
-		lis.append(u)
+	c.execute("UPDATE ziyuanlist SET token = 1 WHERE downlink = ?", (video_m3u8,))
 	conn.commit()
 	conn.close()
-	for i in lis:
-		#判断欲下载的资源是否已经下载,是0说明没下载过
-		if i[2]==0:
-			i0 = str(i[0]).replace(' ','').replace("(","").replace(")","")		#片名
-			path0 = "/root/new_52av/oldvideo/" + i0 + ".mp4"
-			newpath = "/root/new_52av/newvideo/" + i0 + ".mp4"
-			url0 = str(i[1])[0:-5]		#片子m3u8链接
-			download_file(url0, path0,newpath)	
-			conn1 = sqlite3.connect("ziyuan.db")
-			c1 = conn1.cursor()
-			#如果执行了下载，则在数据库中把这个资源标志为已经下载
-			c1.execute("UPDATE ziyuanlist SET token = 1 WHERE name == ?",(i[0],))
-			conn1.commit()
-			conn1.close()
-			comm0 = "ffmpeg -i {0} -acodec copy -vcodec copy -f mp4 -bsf:a aac_adtstoasc {1} -y".format(str(path0),str(newpath))    #转码生成新文件
-			os.system(comm0)
-			while True:
-				comm1 = 'onedrivecmd put "{0}" od:/video/52av/'.format(str(newpath))
-				print(comm1)
-				res = os.popen(comm1)
-				try:
-					clean = str(list(res)[0]).replace("\n","")
-					print(clean)
-				except:
-					clean = "ok"
-				if clean != "Annotations must be specified before other elements in a JSON object":
-					break
-			os.system("rm -rf {0}".format(path0))
-			os.system("rm -rf {0}".format(newpath))
-			#break
-	#    url0 = input("请输入链接:")
-	#    path0 = input("存储地址:")
+	#2.视频转码处理
+	comm = "ffmpeg -i {0} -acodec copy -vcodec copy -f mp4 -bsf:a aac_adtstoasc {1} -y".format(str(oldpath),str(newpath))  # 转码生成新文件
+	os.system(comm)
+
+def upload_file(video_m3u8, oldpath , newpath):
+	while True:
+		comm = 'onedrivecmd put "{0}" od:/'.format(str(newpath))
+		print(comm)
+		res = os.popen(comm)
+		try:
+			clean = str(list(res)[0]).replace("\n", "")
+			print(clean)
+		except:
+			clean = "ok"
+		if clean != "Annotations must be specified before other elements in a JSON object":
+			break
+
+def action(video_m3u8, oldpath, newpath):
+	download_file(video_m3u8, oldpath, newpath)
+	handle_file(video_m3u8, oldpath, newpath)
+	upload_file(video_m3u8, oldpath , newpath)
+	os.system("rm -rf {0}".format(oldpath))
+	os.system("rm -rf {0}".format(newpath))
+
+if __name__ == '__main__':
+
+	#从数据库获取 没被下载过的资源 的结果集，存入list列表，并进行去重！
+	conn = sqlite3.connect("ziyuan.db")
+	c = conn.cursor()
+	db_result = c.execute("select * from ziyuanlist WHERE token = 0 ")
+	result_list = []
+	for u in db_result:
+		result_list.append(u)
+	conn.commit()
+	conn.close()
+	result_list = set(result_list)		#列表去重
+
+	threads_list = []	#线程列表
+	#遍历list列表
+	for i in result_list:
+		video_name = re.sub("[“”（）？，、。！【】]","", str(i[0]))  		#片名
+		prefix0= "F:\\videotutorial\\ceshi\\"
+		prefix = "/root/new_52av/"
+		oldpath = prefix + "oldvideo/" + video_name + ".mp4"
+		newpath = prefix + "newvideo/" + video_name + ".mp4"
+		video_m3u8 = str(i[1])		#片子完整的m3u8链接
+		t = threading.Thread(target=action, args=(video_m3u8,oldpath,newpath))
+		threads_list.append(t)
+
+	for t in threads_list:
+		t.start()
+		while True:
+			# 判断正在运行的线程数量,如果小于5则退出while循环,
+			# 进入for循环启动新的进程.否则就一直在while循环进入死循环
+			if (len(threading.enumerate()) < 5):
+				break
+
